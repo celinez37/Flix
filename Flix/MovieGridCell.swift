933e23c55c6df397bc9b95254d84bb0d00ce316c
celinez37 <cbzhao@ucsd.edu>
2021-09-29 15:09:07 -0700

    I updated a second part to the Flix and also added some new file

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Celine Zhao on 9/27/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet var oldPosterView: UIImageView!
+    
+}
